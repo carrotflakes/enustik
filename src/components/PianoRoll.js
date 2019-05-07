c2ef5d4d40c4f335463509b9976d736cf37da46c
carrotflakes <carrotflakes@gmail.com>
2019-05-07 21:05:40 +0900

    improve pianoroll

--- src/components/PianoRoll.js
+++ src/components/PianoRoll.js
@@ -128,8 +128,10 @@ export default class PianoRoll extends React.Component {
       case 'scrolling': {
         const {x, y} = getPosition(e, this.svg.current);
         const {scrollX, scrollY, scroll} = this.state;
-        const newScrollX = Math.min(0, scrollX + x - scroll.x);
-        const newScrollY = Math.min(0, scrollY + y - scroll.y);
+        const newScrollX = -clamp(-(scrollX + x - scroll.x), 0,
+                                  this.state.widthScale * 100 - (this.props.width-25));
+        const newScrollY = -clamp(-(scrollY + y - scroll.y), 0,
+                                  this.state.heightScale * 128 - (this.props.height-20));
         this.setState({
           scrollX: newScrollX,
           scrollY: newScrollY,
@@ -168,7 +170,7 @@ export default class PianoRoll extends React.Component {
     y -= this.state.scrollY;
     const {durationUnit: {n, d}} = this.state;
     return {
-      notenum: 99 - (y / this.state.heightScale | 0),
+      notenum: 128 - (y / this.state.heightScale | 0),
       tick: ((x - 25) * d / n / this.state.widthScale | 0) * n / d * resolution
     };
   }
@@ -176,7 +178,7 @@ export default class PianoRoll extends React.Component {
   render() {
     const note = event => {
       return <rect x={event.start * this.state.widthScale / resolution}
-                   y={(99 - event.notenum) * this.state.heightScale}
+                   y={(127 - event.notenum) * this.state.heightScale}
                    width={event.duration * this.state.widthScale / resolution - 1}
                    height={this.state.heightScale - 1}
                    key={event.id}
@@ -185,17 +187,29 @@ export default class PianoRoll extends React.Component {
     const notes = this.props.events.map(note);
     this.state.event && notes.push(note(this.state.event));
     const {width, height} = this.props;
-    const piano = Array(100).fill(0).map(
-      (x, i) => <rect x="0" y={this.state.heightScale * i + this.state.scrollY}
+    const piano = Array(128).fill(0).map(
+      (x, i) => <rect x="0" y={this.state.heightScale * (127 - i)}
                       width="24"
                       height={this.state.heightScale - 1}
                       fill={['#DDD', '#777']["010100101010"[i%12]]}
                       key={i}/>);
 
-    const hLines = Array(100).fill(0).map(
+    const pianoMarks = Array(11).fill(0).map(
+      (x, i) => <text x="2" y={this.state.heightScale * ((10 - i) * 12 + 8) - 3}
+                      fontFamily="Verdana" fontSize="12"
+                      stroke="#AAA"
+                      key={i}>C{i-1}</text>);
+
+    const tickMarks = Array(100 / 4).fill(0).map(
+      (x, i) => <text x={i * 4 * this.state.widthScale} y={18}
+                      fontFamily="Verdana" fontSize="12"
+                      stroke="#AAA"
+                      key={i}>{i+1}</text>);
+
+    const hLines = Array(129).fill(0).map(
       (x, i) => <line x1={0} y1={i * this.state.heightScale}
                       x2={10000} y2={i * this.state.heightScale}
-                      stroke={i % 12 ? '#DDD' : '#AAA'}
+                      stroke={(i+4) % 12 ? '#DDD' : '#AAA'}
                       key={i}/>);
     const vLines = Array(100).fill(0).map(
       (x, i) => <line x1={i * this.state.widthScale} y1={0}
@@ -219,10 +233,20 @@ export default class PianoRoll extends React.Component {
            onMouseDown={this.onMouseDown.bind(this)}
            onTouchStart={touchEventWrap(this.onMouseDown).bind(this)}
            ref={this.svg}>
+      <svg viewBox={[0, -this.state.scrollY, 24, height-20].join(' ')}
+           x="0" y="20"
+           width={24} height={height-20}>
       {piano}
-      <svg viewBox={[-this.state.scrollX, -this.state.scrollY, width-25, height].join(' ')}
-      x="25" y="0"
-      width={width-25} height={height}>
+      {pianoMarks}
+      </svg>
+      <svg viewBox={[-this.state.scrollX, 0, width-25, 20].join(' ')}
+           x="25" y="0"
+           width={width-25} height={20}>
+      {tickMarks}
+      </svg>
+      <svg viewBox={[-this.state.scrollX, -this.state.scrollY, width-25, height-20].join(' ')}
+           x="25" y="20"
+           width={width-25} height={height-20}>
       {vLines}
       {hLines}
       {notes}
@@ -252,4 +276,8 @@ function touchEventWrap(eventListener) {
       return eventListener.call(this, event);
     }
   };
+}
+
+function clamp(value, min, max) {
+  return Math.min(max, Math.max(min, value));
 }
