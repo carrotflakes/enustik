a175350c9c02dbf979e561d4ea6f8476e3260bf3
carrotflakes <carrotflakes@gmail.com>
2019-06-10 23:48:48 +0900

    removed redux-logger, use Redux DevTools good:)

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { all } from 'redux-saga/effects';
-import logger from 'redux-logger';
 
 export default function configureStore(reducers, saga) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -9,7 +8,7 @@ export default function configureStore(reducers, saga) {
 
   const store = createStore(
     combineReducers(reducers),
-    composeEnhancers(applyMiddleware(logger, sagaMiddleware)));
+    composeEnhancers(applyMiddleware(sagaMiddleware)));
 
   sagaMiddleware.run(saga);
 
